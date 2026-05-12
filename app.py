b22a4c958ae651d25b4a2c97da7accf4b5e4e096
Vaibhav Pal <vaibhavkumar8979581578@gmail.com>
2026-05-12 21:49:18 +0530

    change in app.py file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return " aspiring DevOps engineer"
+    return " aspiring DevOps engineer ~ "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
